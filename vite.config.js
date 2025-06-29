2f9b5c3eaa5d933b004922dbdd22033b199c3807
Panji <panjipusaka71@gmail.com>
2025-06-29 13:37:43 +0700

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,8 @@ export default defineConfig({
             "@": resolve(__dirname, "./src"),
         },
     },
+    server: {
+        allowedHosts: ["nsl.panjip.my.id"],
+        host: true,
+    },
 });
